2e94b3d1a360cdab91147dc09181f2bb82b5ae7c
alpit-g <alpit.gajbhiye@smartdatainc.net>
2019-07-12 14:45:34 +0530

    DeleteChartOfAccount

--- HumanitarianAssistance.Application/Accounting/Commands/Delete/DeleteChartOfAccountQuery.cs
+++ HumanitarianAssistance.Application/Accounting/Commands/Delete/DeleteChartOfAccountQuery.cs
@@ -1,7 +1,10 @@
+using HumanitarianAssistance.Application.Infrastructure;
+using MediatR;
+
 namespace HumanitarianAssistance.Application.Accounting.Commands.Delete
 {
-    public class DeleteChartOfAccountQuery
+    public class DeleteChartOfAccountQuery : BaseModel, IRequest<ApiResponse>
     {
-        
+        public long AccountId { get; set; }
     }
 }
