e1bebb63a2bf4bab1514fe1376f8a11c9bd6e0ab
alpit-g <alpit.gajbhiye@smartdatainc.net>
2019-07-14 21:25:58 +0530

    changes in marketing

---
+++ HumanitarianAssistance.Application/Marketing/Commands/Delete/DeleteLanguageCommand.cs
@@ -0,0 +1,10 @@
+using HumanitarianAssistance.Application.Infrastructure;
+using MediatR;
+
+namespace HumanitarianAssistance.Application.Marketing.Commands.Delete
+{
+    public class DeleteLanguageCommand : BaseModel,IRequest<ApiResponse>
+    {
+        public int LanguageId { get; set; }        
+    }
+}
