d9cc8b6144e51267e890c99493042a16c65870d0
Saksham Srivastava <Saksham Srivastava@DESKTOP-ETMF01D>
2019-08-01 11:03:37 +0530

    Changes in Marketing

---
+++ HumanitarianAssistance.Application/Marketing/Commands/Common/AddEditPolicyCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using HumanitarianAssistance.Application.Infrastructure;
+using HumanitarianAssistance.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HumanitarianAssistance.Application.Marketing.Commands.Common
+{
+    public class AddEditPolicyCommandHandler : IRequestHandler<AddEditActivityTypeCommand, ApiResponse>
+    {
+        private HumanitarianAssistanceDbContext _dbContext;
+        private IMapper _mapper;
+        public AddEditPolicyCommandHandler(HumanitarianAssistanceDbContext dbContext,IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+        public Task<ApiResponse> Handle(AddEditActivityTypeCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HumanitarianAssistance.WebApi/Controllers/Marketing/PolicyController.cs
@@ -0,0 +1,48 @@
+﻿using HumanitarianAssistance.Application.Infrastructure;
+using HumanitarianAssistance.Application.Marketing.Commands.Common;
+using HumanitarianAssistance.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace HumanitarianAssistance.WebApi.Controllers.Marketing
+{
+    [Produces("application/json")]
+    [Route("api/Policy/[Action]")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class PolicyController : Controller
+    {
+        private readonly JsonSerializerSettings _serializerSettings;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IMediator _mediator;
+        public PolicyController(UserManager<AppUser> userManager, IMediator mediator)
+        {
+            _userManager = userManager;
+            _mediator = mediator;
+            _serializerSettings = new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                NullValueHandling = NullValueHandling.Ignore
+            };
+        }
+
+        [HttpPost]
+        public async Task<ApiResponse> AddEditPolicy([FromBody]AddEditPolicyCommand command)
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            command.ModifiedById = userId;
+            command.ModifiedDate = DateTime.UtcNow;
+            command.CreatedById = userId;
+            command.CreatedDate = DateTime.UtcNow;
+            return await _mediator.Send(command);
+        }
+    }
+}
