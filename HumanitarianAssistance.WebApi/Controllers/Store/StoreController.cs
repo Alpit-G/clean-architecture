460a75de187b3a754b03c6ee3099aff239566665
arjuns <arjuns@SDD-1>
2019-08-08 13:04:02 +0530

    chnages in store

---
+++ HumanitarianAssistance.WebApi/Controllers/Store/StoreController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using HumanitarianAssistance.Common.Enums;
+
+namespace HumanitarianAssistance.WebApi.Controllers.Store
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/Store/[Action]")]
+    [ApiExplorerSettings(GroupName = nameof(SwaggerGrouping.Store))]
+    [Authorize]
+    public class StoreController : BaseController
+    {
+
+
+    }
+}
