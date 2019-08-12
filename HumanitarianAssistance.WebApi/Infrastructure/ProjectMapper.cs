f536d39997d3031eabbae8a8b37eb77081bf8c9d
avinashr <avinashr@DESKTOP-6OB5DTP>
2019-08-12 14:36:50 +0530

    Added Multiple automapper profile classes

---
+++ HumanitarianAssistance.WebApi/Infrastructure/ProjectMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using HumanitarianAssistance.Application.Project.Commands.Common;
+using HumanitarianAssistance.Domain.Entities.Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HumanitarianAssistance.WebApi.Infrastructure
+{
+    public class ProjectMapper : Profile
+    {
+        public ProjectMapper()
+        {
+            CreateMap<AddEditProjectDetailCommand, ProjectDetail>().ReverseMap();
+        }
+    }
+}
